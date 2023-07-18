d0f8775bfbce219ae1ef59a9e3c88bf32ad3b97f
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-18 20:12:28 +0530

    frontend adding image and basic css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css'
 
-function Greeting() {
-    return <h1>welcome to react tutorials</h1>
+const BookList = () => {
+  return <section className="booklist">
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+ 
+  </section>;
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Greeting/>)
+const Book = () => {
+    return <article className="book">
+        <Image/>
+        <Title/>
+        <Author/>
+
+    </article>;
+}
+
+const Image = () =>( 
+<img src="https://images-eu.ssl-images-amazon.com/images/I/71CrTyKsazL._AC_UL600_SR600,400_.jpg" alt="Indian Polity"/>);
+const Title = () => <h2>Indian Polity</h2>
+const Author = () => {
+    return <h4>M laxmikanth</h4>
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
