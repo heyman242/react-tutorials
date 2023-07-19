4cde7afd2474fbbf86d36d1785d6281d2c38511c
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-19 20:06:26 +0530

    frontend react data flow

--- src/index.js
+++ src/index.js
@@ -1,33 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'
+import "./index.css";
+
+const books = [
+  {
+    author: "M laxmikanth",
+    title: "Indian Polity",
+    src: "https://images-eu.ssl-images-amazon.com/images/I/71CrTyKsazL._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    author: "James CLear",
+    title: "Atomic Habits",
+    src: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL300_SR300,200_.jpg",
+    id: 2,
+  },
+];
 
 const BookList = () => {
-  return <section className="booklist">
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
- 
-  </section>;
-}
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} getBook={getBook} />;
+      })}
+    </section>
+  );
+};
 
-const Book = () => {
-    return <article className="book">
-        <Image/>
-        <Title/>
-        <Author/>
-
-    </article>;
-}
-
-const Image = () =>( 
-<img src="https://images-eu.ssl-images-amazon.com/images/I/71CrTyKsazL._AC_UL600_SR600,400_.jpg" alt="Indian Polity"/>);
-const Title = () => <h2>Indian Polity</h2>
-const Author = () => {
-    return <h4>M laxmikanth</h4>
-}
-
+const Book = (props) => {
+  const { src, title, author, getBook, id } = props;
+  const getSingleBook = () => {
+    getBook(id);
+  };
+  return (
+    <article className="book">
+      <img src={src} alt={title} />
+      <h2>{title}</h2>
+      <button onClick={getSingleBook}>display title</button>
+      <h4>{author.toUpperCase()}</h4>
+    </article>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
